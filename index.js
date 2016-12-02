8508698c0d1e85ea2c4e84d044ea39dee20c5d4e
Bogdan Paliy <bpalij@gmail.com>
2016-12-02 16:54:00 +0200

    Change buttons

--- index.js
+++ index.js
@@ -1,27 +1,4 @@
-$('#rst-btn').mouseenter(function (){
-	$(this).addClass("btn-danger");
-	$(this).removeClass("btn-warning");
-});
-$('#rst-btn').mouseleave(function (){
-	$(this).removeClass("btn-danger");
-	$(this).addClass("btn-warning");
-});
-$('#start-button').mouseenter(function (){
-	$(this).addClass("btn-primary");
-	$(this).removeClass("btn-success");
-});
-$('#start-button').mouseleave(function (){
-	$(this).removeClass("btn-primary");
-	$(this).addClass("btn-success");
-});
-$('#start-boolean').mouseenter(function (){
-	$(this).addClass("btn-primary");
-	$(this).removeClass("btn-success");
-});
-$('#start-boolean').mouseleave(function (){
-	$(this).removeClass("btn-primary");
-	$(this).addClass("btn-success");
-});
+
 // function isWhole(num){
 // 	return (num-Math.floor(num)!==0 && !isNan(num));
 // }
